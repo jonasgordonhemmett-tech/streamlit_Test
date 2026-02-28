85bbf2a74008b5739ccc9dd4794ad4aef0960654
JonasH <johannes.hemmett@uvm.edu>
2026-02-27 20:45:38 -0500

    Start

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import streamlit as st
-st.write("Hello world")
+
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
 st.html("""
 <h1>Hello World</h1>
 """)
+
+userNum = st.text_input("Enter an integer: ")
+if userNum:
+    try:
+        int(userNum)
+        st.write(f"Your number is {userNum}")
+    except:
+        st.write("Please enter a valid number")
