f3361c8d2e50d0f4db6d9c054af3d50520fabd1f
JonasH <johannes.hemmett@uvm.edu>
2026-02-27 21:08:00 -0500

    Work

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
+
 st.html("""
 <h1>Hello World</h1>
 """)
 
+def isPrime(n, divisor=2):
+    if n < 2:
+        return False
+    if divisor ** 2 > n:
+        return True
+    if n % divisor:
+        return False
+    return isPrime(n, divisor+1)
+
 userNum = st.text_input("Enter an integer: ")
+
 if userNum:
     try:
-        int(userNum)
-        st.write(f"Your number is {userNum}")
+        userNum = int(userNum)
+        st.write(f"Your integer is {userNum}")
+        st.write(f"{userNum} is {"prime" if isPrime(userNum) else "not prime"}.")
+
+
     except:
-        st.write("Please enter a valid number")
+        st.write("Please enter a valid integer number")
+    
